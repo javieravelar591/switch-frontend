f66bbf3f81e1a2358b3c65a2209c10a2a42334f7
Javier Avelar <javieravelar59@gmail.com>
2025-11-09 13:45:18 -0600

    slight changes

--- src/components/brands/BrandCard.tsx
+++ src/components/brands/BrandCard.tsx
@@ -13,13 +13,13 @@ export default function BrandCard({ brand }: BrandCardProps) {
         <div className="bg-white rounded-2xl shadow p-4 flex flex-col items-center hover:scale-105 transition">
         {brand.logo_url && (
             <img
-            src={brand.logo_url}
-            alt={brand.name}
-            className="h-24 w-24 object-contain mb-4"
+                src={brand.logo_url}
+                alt={brand.name}
+                className="h-24 w-24 object-contain mb-4"
             />
         )}
-        <h2 className="font-semibold text-lg">{brand.name}</h2>
-        <p className="text-sm text-gray-600 text-center mt-2 line-clamp-3">
+        <h2 className="font-semibold text-lg text-black">{brand.name}</h2>
+        <p className="text-sm text-black-600 text-center mt-2 line-clamp-3">
             {brand.description}
         </p>
         </div>
