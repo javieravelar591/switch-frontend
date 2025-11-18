f3061442158b46ffb459f8186e4cfb543a30a3c5
javieravelar591 <javieravelar59@gmail.com>
2025-11-17 22:35:32 -0500

    add some animations, styling, make look a bit better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,10 @@ import MasonryGrid from "@/components/masonry/MasonryGrid";
 import BrandsPage from "@/pages/brands";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:8000/brands", {
-    cache: "no-store",
-  });
-  const brands = await res.json();
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {/* <BrandsPage /> */}
-      <MasonryGrid items={brands} />
+      <MasonryGrid />
     </div>
   );
 }
