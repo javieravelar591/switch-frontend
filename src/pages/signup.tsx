33eb728e9c4a53e216bf264d1c7f0a4fc00b9623
javieravelar591 <javier.avelar99@gmail.com>
2026-03-06 18:23:25 -0500

    update route removed rediirect slash from be

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -23,7 +23,7 @@ export default function SignupPage() {
     setError(null);
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
